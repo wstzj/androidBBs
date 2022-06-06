53d49cbec5ccb6ea6fb9d71a0edd44131ddda58c
TangZ <735647571@qq.com>
2022-06-06 12:04:41 +0800

    添加User_Role数据表查找方法

---
+++ src/main/java/com/example/androidbbs/Repository/UserRoleRepository.java
@@ -0,0 +1,37 @@
+package com.example.androidbbs.Repository;
+
+import com.example.androidbbs.Entity.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+
+    /**
+     * 查找某一User的所有Role
+     * userId:Long
+     */
+    List<UserRole> findUserRolesByUserId(Long userId);
+
+    /**
+     * 查找某一Role的所有User
+     * roleId:Long
+     */
+    List<UserRole> findUserRolesByRoleId(Long roleId);
+
+    /**
+     * 删除User所有Role
+     * userId:Long
+     */
+    boolean deleteUserRolesByUserId(Long userId);
+
+    /**
+     * 删除User指定Role
+     * userId:Long
+     * roleId:Long
+     */
+    boolean deleteUserRoleByUserIdAndRoleId(Long userId, Long roleId);
+
+}
